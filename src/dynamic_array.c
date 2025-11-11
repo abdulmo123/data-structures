fb52c0da29478b104eeeb86348755ae62a71caf6
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2025-11-11 17:01:48 -0600

    code cleanup + better comments + add some bounds checking

--- src/dynamic_array.c
+++ src/dynamic_array.c
@@ -29,7 +29,6 @@ int main() {
 	// call isEmpty method
 	isEmpty(&arr);
 	// call PUSH method :)
-	//push(&arr, 4);
 	for (int i = 0; i < 16; i++) {
 		int r = rand() % 20;
 		push(&arr, r);
@@ -49,10 +48,12 @@ int main() {
 	// call INSERT method
 	insert(&arr, 3, 5);
 	insert(&arr, 0, -1);
+
 	// call REMOVE method
 	removeItem(&arr, 2);
 	removeItem(&arr, 0);
 	removeItem(&arr, 4);
+
 	return 0;
 }
 
@@ -61,7 +62,7 @@ void init(DynamicArray *arr) {
 	arr->capacity = 4; // can also do: (*arr).capacity = 4;
 	arr->data = malloc(arr->capacity * sizeof(int)); // allocate spsace for 4 ints on init
 
-	printf("Init array ...");
+	printf("Initialize the array ...\n");
 	for (int i = 0; i < arr->capacity; i++) {
 		printf("%d ", arr->data[i]); 
 	}
@@ -69,25 +70,24 @@ void init(DynamicArray *arr) {
 }
 
 void push(DynamicArray *arr, int value) {
+	// resize array if not enough capacity allocated in memory
 	if (arr->size == arr->capacity) {
 		int newCapacity = arr->capacity * 2;
 		arr->capacity = newCapacity;
 		int *newData = realloc(arr->data, newCapacity * sizeof(int));
 		if (newData == NULL) {
-			printf("Failed. Unable to resize memory");
 			return;
 		}
 		else {
-			printf("Success!. %lu bytes allocated at address %p!\n", arr->capacity * sizeof(int), newData);
 			arr->data = newData;
-		}
-		printf("New capacity yay! :) %d\n", arr->capacity);
+			printf("Success!. %lu bytes allocated at address %p!\n", arr->capacity * sizeof(int), arr->data);
+		};
 	}
 
 	arr->data[arr->size] = value;
 	arr->size++;
 
-	printf("After PUSHING ...\n");
+	printf("PUSHING value: {%d} to the END of the array ...\n", value);
 	printf("Size = %d\n", arr->size);
 	printf("Capacity = %d\n", arr->capacity);
 	for (int i = 0; i < arr->capacity; i++) {
@@ -97,26 +97,23 @@ void push(DynamicArray *arr, int value) {
 }
 
 void pop(DynamicArray *arr) {
-	// todo: check if size is less than capacity / 2 ---> realloc smaller arr
-	// do i need to actually 'remove' the values? or just set to 0 and reduce size
+	// realloc to smaller array if capacity too larger (don't want to waste memory)
 	if (arr->size * 2 <= arr->capacity) {
-		// todo: realloc smaller arr
 		int newCapacity = arr->capacity / 2;
 		int *newData = realloc(arr->data, (arr->capacity / 2) * sizeof(int));
 		if (newData == NULL) {
-			printf("NOOOO!!");
 			return;
 		} else {
 			arr->data = newData;
 			arr->capacity = newCapacity;
-			printf("YAAAAAAAAAY!!!");
+			printf("Success! {%lu} bytes allocated at address: {%p}\n", arr->capacity * sizeof(int), arr->data);
 		}
 	}
 
 	arr->data[arr->size - 1] = 0;
 	arr->size--;
 
-	printf("After POPPING ...\n");
+	printf("POPPING from the END of the array ...\n");
 	printf("Size = %d\n", arr->size);
 	printf("Capacity = %d\n", arr->capacity);
 	for (int i = 0; i < arr->capacity; i++) {
@@ -126,32 +123,46 @@ void pop(DynamicArray *arr) {
 }
 
 int search(DynamicArray *arr, int value) {
+	// if array length is 0, nothing to search so value is not found
 	if (arr->size == 0) {
 		return -1;
 	}
 
+	// if value found in array, return it
 	for (int i = 0; i < arr->size; i++) {
 		if (arr->data[i] == value) {
-			printf("Found the value: {%d} at index {%d}!!!\n", value, i);
+			printf("Found the value: {%d} at index {%d}!\n", value, i);
 			return i;
 		}
 	}
 
+	// return -1 if value is not found
 	printf("Value {%d} not found in array :(\n", value);
 	return -1;
 }
 
 bool isEmpty(DynamicArray *arr) {
+	// returns a 0 (FALSE) or 1 (TRUE) based on if array is empty or not
 	printf("Is array empty? %d\n", arr->size == 0);
 	return arr->size == 0;
 }
 
 void insert(DynamicArray *arr, int index, int value) {
+	// check to make sure index is not out of bounds before trying to insert
+	if (index > arr->size || index < 0) {
+		printf("Index is out of bounds! Cannot INSERT value: {%d} at index: {%d}\n", value, index);
+	}
+
+	// realloc to larger capacity in memory
 	if (arr->size == arr->capacity) {
-		// realloc size to larger capacity
 		int *newData = realloc(arr->data, (arr->capacity * 2) * sizeof(int));
-		arr->capacity = arr->capacity * 2;
-		arr->data = newData;
+		if (newData == NULL) {
+			return;
+		} else {
+			arr->capacity = arr->capacity * 2;
+			arr->data = newData;
+			printf("Success! {%lu} bytes allocated at address: {%p}\n", arr->capacity * sizeof(int), arr->data);
+		}
 	}
 
 	// shift items to the right up until we reach index
@@ -159,10 +170,11 @@ void insert(DynamicArray *arr, int index, int value) {
 		arr->data[i] = arr->data[i-1];
 	}
 
+	// insert value at index and increment size
 	arr->data[index] = value;
 	arr->size++;
 
-	printf("After INSERTING value: {%d} at index: {%d}...\n", value, index);
+	printf("INSERTING value: {%d} at index: {%d}...\n", value, index);
 	printf("Size = %d\n", arr->size);
 	printf("Capacity = %d\n", arr->capacity);
 	for (int i = 0; i < arr->capacity; i++) {
@@ -172,6 +184,12 @@ void insert(DynamicArray *arr, int index, int value) {
 }
 
 void removeItem(DynamicArray *arr, int index) {
+	// check to make sure index is not out of bounds before trying to remove
+	if (index > arr-> size || index < 0) {
+		printf("Index is out of bounds! Cannot REMOVE item at index: {%d}\n", index);
+	}
+
+	// realloc to smaller capacity in memory to prevent wasted space in memory
 	if (arr->size * 2 <= arr->capacity) {
 		int newCapacity = arr->capacity / 2;
 		int *newData = realloc(arr->data, newCapacity * sizeof(int));
@@ -180,10 +198,14 @@ void removeItem(DynamicArray *arr, int index) {
 		} else {
 			arr->data = newData;
 			arr->capacity = newCapacity;
+			printf("Success! {%lu} bytes allocated at address: {%p}\n", arr->capacity * sizeof(int), arr->data);
 		}
 	}
 
+	// set to-be-removed item to 0
 	arr->data[index] = 0;
+
+	// swap removed item all the way to end of array and then decrement size
 	for (int i = index; i < arr->size - 1; i++) {
 		int temp = arr->data[i];
 		arr->data[i] = arr->data[i+1];
@@ -191,7 +213,7 @@ void removeItem(DynamicArray *arr, int index) {
 	}
 	arr->size--;
 
-	printf("After REMOVING item at index: {%d}\n", index);
+	printf("REMOVING item at index: {%d}\n", index);
 	printf("Size = %d\n", arr->size);
 	printf("Capacity = %d\n", arr->capacity);
 	for (int i = 0; i < arr->capacity; i++) {
